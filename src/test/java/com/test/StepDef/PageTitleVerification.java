3595c026d58c8970622234931d378a1bbad18c77
Warrior <mr.g.sathyaraj@gmail.com>
2021-08-25 18:37:24 +0530

    added maven options

---
+++ src/test/java/com/test/StepDef/PageTitleVerification.java
@@ -0,0 +1,27 @@
+package com.test.StepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+
+public class PageTitleVerification {
+
+
+
+    @Given("Navigate to the HomePage")
+    public void navigate_to_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Verify the Homepage Title")
+    public void verify_the_homepage_title() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ target/test-classes/Features/AmazonPageTitleVerification.feature
@@ -0,0 +1,4 @@
+Feature: Verify Amazon Page Title
+  Scenario: Test Homepage title
+  Given Navigate to the HomePage
+  Then Verify the Homepage Title
